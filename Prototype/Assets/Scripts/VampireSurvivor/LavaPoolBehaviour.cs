eca8d5981f5bb62bcf297dec0f260116901222d6
Aleksandra Zupanska <aleksandra.zupanska@student.howest.be>
2024-08-31 16:26:29 +0200

    bullet vfx and lava pool vfx

--- Prototype/Assets/Scripts/VampireSurvivor/LavaPoolBehaviour.cs
+++ Prototype/Assets/Scripts/VampireSurvivor/LavaPoolBehaviour.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class LavaPoolBehaviour : MonoBehaviour
 {
-    public float TimeToDie = 6, Damage = 10;
+    public float TimeToDie = 6, Damage = 10, id;
     private float _timer = 0f, _dmgTimer = 0;
+    private VisualEffect _visualEffect;
+
+    private void Start()
+    {
+        _visualEffect = GetComponent<VisualEffect>();
+        id = Shader.PropertyToID("Duration");
+        _visualEffect.SetFloat((int)id, TimeToDie);
+    }
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Enemy")
